1d917b4e48e5db108d03bdb27d698cf914f039d4
Colin Sandman <xx56sandmanxx@gmail.com>
2022-10-27 16:51:30 -0400

    Adding in Player Movement, including first person camera, player jumping, and movement

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] private Transform cameraPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPos.position;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private Transform orientation;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+
+    float horizontal;
+    float vertical;
+    float groundDistance = 0.4f;
+    bool isGrounded;
+    float walkSpeed = 8f;
+    float sprintSpeed = 12f;
+    float moveSpeed;
+
+
+    Vector3 moveDir;
+    Vector3 velocity;
+    CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        moveSpeed = walkSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Debug.Log(isGrounded);
+        Debug.Log(velocity.y);
+
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        moveDir = orientation.forward * vertical + orientation.right * horizontal;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else
+        {
+            moveSpeed = walkSpeed;
+        }
+
+        characterController.Move(moveDir * moveSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
