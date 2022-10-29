34c26787d0b1478fcf1fe43dbec60c552be71664
Sam Lowry <lowrysa@email.sc.edu>
2022-10-28 20:38:52 -0400

    first cutscene

---
+++ Assets/Scripts/Dialouge.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+ [Serializable]
+public class Dialouge : MonoBehaviour
+{
+	private string[] sentences;
+    // Start is called before the first frame update
+}
