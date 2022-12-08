110eb44869d24dd628f973f66876fbd908e41947
ddkeen <d3keen@gmail.com>
2022-12-08 13:47:47 -0500

    Music for menu, cutscence, levels

---
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/cutsceneBGM.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cutsceneBGM : MonoBehaviour
+{
+    public static cutsceneBGM BgMInstance;
+    
+    private void Awake()
+    {
+		if(BgMInstance != null && BgMInstance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+		
+		BgMInstance = this;
+		DontDestroyOnLoad(this);
+	}
+}
+
